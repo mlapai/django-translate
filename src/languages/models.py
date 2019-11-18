9ad231e50ef0e1e3705c4257f65a4bf3a7d71db3
Mladen Paic <paic.m@live.com>
2019-11-19 00:16:24 +0100

    translate option model

--- src/languages/models.py
+++ src/languages/models.py
@@ -7,3 +7,13 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+class TranslateOption(models.Model):
+    from_language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='from_language')
+    to_language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='to_language')
+    essential_price_per_word = models.DecimalField(max_digits=6, decimal_places=2)
+    professional_price_per_word = models.DecimalField(max_digits=6, decimal_places=2)
+    premium_price_per_word = models.DecimalField(max_digits=6, decimal_places=2)
+    rush_price_percentage = models.DecimalField(max_digits=3, decimal_places=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
