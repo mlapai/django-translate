03c55568028ac3b1eff831a8ba5e8ab686738c43
Mladen Paic <paic.m@live.com>
2019-11-19 00:24:43 +0100

    django seeder bootstrap

--- src/config/common.py
+++ src/config/common.py
@@ -29,6 +29,7 @@ class Common(Configuration):
         'rest_framework_swagger',     # swagger api
         'versatileimagefield',        # image lib
         'social_django',              # social login
+        'django_seed'                 # seeder
 
         # Your apps
         'src.users',
