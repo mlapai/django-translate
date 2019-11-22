ba0799ec6044e2391a012752a4e8e0d8799e0aef
Mladen Paic <paic.m@live.com>
2019-11-22 01:53:41 +0100

    language models validation

--- src/languages/serializers.py
+++ src/languages/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
 from .models import Language, TranslateOption
 
 
 class LanguageSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=50)
+
     class Meta:
         model = Language
         fields = ('id', 'name')
 
 class TranslateOptionSerializer(serializers.ModelSerializer):
+    essential_price_per_word = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=0.01)
+    professional_price_per_word = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=0.01)
+    premium_price_per_word = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=0.01)
+
     class Meta:
         model = TranslateOption
         fields = (
